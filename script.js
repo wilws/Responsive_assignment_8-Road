f44996448ad0904d33642261d189e916b1ad2559
wilws <wilson.ws.pro@gmail.com>
2022-07-07 08:59:06 +0100

    firsh

---
+++ script.js
@@ -0,0 +1,29 @@
+const container = document.querySelector(".container");
+
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+  container.classList.add("change");
+});
+
+document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+  container.classList.remove("change");
+});
+
+const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
+
+
+
+// const colors = ["#6495ed","#7fffd4","#ffa07a","#f08080","#af3333"];
+let i = 0;
+
+document.querySelectorAll(".nav-link").forEach((item)=>{
+    item.style.cssText = `background-color:${colors[i++]}`
+});
+
+
+// Card Change
+
+Array.from(document.querySelectorAll(".navigation-button")).forEach((item)=>{
+    item.onclick=()=>{
+        item.parentElement.parentElement.classList.toggle("change");
+    }
+});
